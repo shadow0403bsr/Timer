8d3b35e777318bdd5b4067bdd911588a5c6d7264
Baljinder Singh <25747675+shadow0403bsr@users.noreply.github.com>
2019-04-04 10:13:47 +0530

    New theme

---
+++ js/timeZone.js
@@ -0,0 +1,18 @@
+var acc = document.getElementsByClassName("tzbar accordion");
+var panel = document.getElementsByClassName('panel');
+for (var i = 0; i < acc.length; i++) {
+	acc[i].onclick = function() {
+		var setClasses = !this.classList.contains('active');
+		setClass(acc, 'active', 'remove');
+		setClass(panel, 'show', 'remove');
+		if (setClasses) {
+			this.classList.toggle("active");
+			this.nextElementSibling.classList.toggle("show");
+		}
+    }
+}
+function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+    	els[i].classList[fnName](className);
+    }
+}
